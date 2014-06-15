d506868ce7c541c56a367b4b7e54dc7cdb5de4a4
Azruine <tyriba967@gmail.com>
2014-06-16 06:10:51 +0900

    201406160610

--- Tetris/Block.h
+++ Tetris/Block.h
@@ -1,22 +1,136 @@
 #include <iostream>
+#include <string.h>
 
 class Block
 {
 public:
-	Block(){}
+	Block()
+	{
+		for (int i = 0; i < 4; i++)
+		{
+			memset(this->blockShape[i], 0, sizeof(int)*4);
+		}
+	}
 	~Block(){}
 
-	void print()
+	void changeValue(int i, int j)
+	{
+		this->blockShape[i][j] = 1;
+
+		return;
+	}
+	
+	int print()			//화면 출력
 	{
 		for (int i = 0; i < 16; i++)
 		{
-			if (this->Blockshape[i / 4][i % 4] == 1)
+			if (this->blockShape[i / 4][i % 4] == 1)
 			{
-				std::cout << "" << std::endl;
+				std::cout << "■" << std::endl;
+			}
+			else
+			{
+				std::cout << "□" << std::endl;
 			}
 		}
+
+		return 1;
+	}
+
+	int turnRight()		//오른쪽 방향 클릭 회전
+	{
+		int blockShapeTemp[4][4];
+
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				blockShapeTemp[i][j] = this->blockShape[i][j];
+			}
+		}
+
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				this->blockShape[i][j] = blockShape[j][3 - i];
+			}
+		}
+
+		return 1;
+	}
+
+	int turnLeft()		//왼쪽 방향 클릭 회전
+	{
+		int blockShapeTemp[4][4];
+
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				blockShapeTemp[i][j] = this->blockShape[i][j];
+			}
+		}
+
+		for (int i = 0; i < 4; i++)
+		{
+			for (int j = 0; j < 4; j++)
+			{
+				this->blockShape[i][j] = blockShape[3-j][i];
+			}
+		}
+
+		return 1;
+	}
+
+	int removeVoid()		//최하단 공백 제거 함수
+	{
+		bool checkZero = true;
+
+		int i = 0, j = 0; //루프 변수
+
+		int blockShapeTemp[4][4];
+
+		while (true)
+		{
+
+			for (j = 0; j < 4; j++)
+			{
+				checkZero = checkZero && (this->blockShape[i][j] = 0);
+			}
+
+
+			if (!checkZero)
+			{
+				break;
+			}
+
+			for (i = 0; i < 4; i++)		//임시변수 초기화
+			{
+				for (j = 0; j < 4; j++)
+				{
+					blockShapeTemp[i][j] = this->blockShape[i][j];
+				}
+			}
+
+			for (i = 0; i < 4; i++)			//마지막 줄을 제일 위로 올림
+			{
+				this->blockShape[0][i] = blockShapeTemp[3][i];
+			}
+
+			for (i = 0; i < 3; i++)		//남은 세줄을 한줄씩 내림
+			{
+				for (j = 0; j < 4; j++)
+				{
+					this->blockShape[i + 1][j] = blockShapeTemp[i][j];
+				}
+			}
+		}
+
+		return 1;
+
 	}
 
 private:
-	int** Blockshape[4][4];
+	int blockShape[4][4];
 };
--- Tetris/main.cpp
+++ Tetris/main.cpp
@@ -1,7 +1,30 @@
 #include "Block.h"
+#include <time.h>
+
+int storeFunc(Block store, Block present)
+{
+	Block temp;
+
+	temp = present;
+
+	present = store;
+
+	store = temp;
+
+}
 
 int main(void)
 {
+	srand((unsigned int)time(NULL)); //난수 시드
+
+	int i = 0, j = 0;
+
+	Block arBlock[7];
+
+	Block store, next, present;
+
+	int isStored = 0;
+
 	std::cout << "▨▤▤▤▤▤▤▤▤▤▤▤▧" << std::endl;
 
 	for (int i = 0; i < 20; i++)
@@ -10,6 +33,54 @@ int main(void)
 	}
 
 	std::cout << "▧▤▤▤▤▤▤▤▤▤▤▤▨" << std::endl;
+
+
+	for (i = 0; i < 4; i++)		//I shape
+	{
+		arBlock[0].changeValue(4, i);
+	}
+
+	for (i = 0; i < 3; i++)		//T shape
+	{
+		arBlock[1].changeValue(4, i);
+	}
+	arBlock[1].changeValue(3, 1);
+
+	for (i = 0; i < 3; i++)		//L shape
+	{
+		arBlock[2].changeValue(4, i);
+	}
+	arBlock[2].changeValue(3, 0);
+
+	for (i = 0; i < 3; i++)		//J shape
+	{
+		arBlock[3].changeValue(4, i);
+	}
+	arBlock[3].changeValue(3, 2);
+
+	for (i = 0; i < 2; i++)		//S shape
+	{
+		arBlock[4].changeValue(4, i);
+	}
+	arBlock[4].changeValue(3, 1);
+	arBlock[4].changeValue(3, 2);
+
+	for (i = 1; i < 3; i++)		//Z shape
+	{
+		arBlock[5].changeValue(4, i);
+	}
+	arBlock[5].changeValue(3, 0);
+	arBlock[5].changeValue(3, 1);
+
+	for (i = 0; i < 2; i++)		//O shape
+	{
+		arBlock[6].changeValue(4, i);
+	}
+	arBlock[6].changeValue(3, 0);
+	arBlock[6].changeValue(3, 1);
+	
+
+	
 
 	
 }
